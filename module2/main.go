89e7e5476cc499e5d45ce2e875d7da65d1d4e6a2
xiaodongma9 <xiaodong.ma@liveramp.com>
2022-07-17 21:02:29 +0800

    feature: module8

--- module2/main.go
+++ module2/main.go
@@ -5,11 +5,15 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
 	"strings"
+	"syscall"
+	"time"
 )
 
 func main() {
 	os.Setenv("version", "0.1")
+	go trapShutdownSignal()
 	http.HandleFunc("/response-header/request", setResHeaderWithReq)
 	http.HandleFunc("/response-header/sysenv", setResHeaderWithSysVersion)
 	http.HandleFunc("/info-logging", logInfo)
@@ -18,6 +22,23 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func trapShutdownSignal() {
+	log.Println("watching for termination signals")
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGTERM)
+
+	// when we get a signal, flip the global ShuttingDown flag
+	sig := <-sigChan
+	log.Println("got signal:", sig)
+
+	// wait for the liveness checks to fail and kubernetes to reconfigure
+	log.Println("graceful shutdown has begun")
+	time.Sleep(time.Second * 20)
+	// sleep while the cluster removes this instance from incoming service traffic
+	log.Println("exiting clean due to shutdown signal")
+	os.Exit(0)
 }
 
 func setResHeaderWithReq(w http.ResponseWriter, r *http.Request) {
@@ -32,7 +53,7 @@ func setResHeaderWithSysVersion(w http.ResponseWriter, r *http.Request) {
 }
 
 func logInfo(w http.ResponseWriter, r *http.Request) {
-	ip := r.RemoteAddr
+	ip := r.Header.Get("X-REAL-IP")
 	log.Printf("client IP: %s\n", ip)
 	log.Printf("httpCode %d\n", http.StatusOK)
 	w.Write([]byte(ip))
